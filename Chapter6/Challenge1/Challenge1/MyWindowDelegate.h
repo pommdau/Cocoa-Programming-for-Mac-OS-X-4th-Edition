25c17f10200c5092ddf19ec5da3d6bdcf299ff90
hikeuchi <hikeuchi@morisawa.co.jp>
2019-02-18 16:57:17 +0900

    Chapter6/P137

---
+++ Chapter6/Challenge1/Challenge1/MyWindowDelegate.h
@@ -0,0 +1,14 @@
+//
+//  MyWindowDelegate.h
+//  Challenge1
+//
+//  Created by HIROKI IKEUCHI on 2019/02/18.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface MyWindowDelegate : NSObject <NSWindowDelegate>
+
+@end

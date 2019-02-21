c3e2400cef3fca7a8b8bafe751396248c194c3bb
hikeuchi <hikeuchi@morisawa.co.jp>
2019-02-21 13:58:49 +0900

    P244

---
+++ Chapter12/CarLot/CarLot/Preference/PreferenceController.m
@@ -0,0 +1,41 @@
+//
+//  PreferenceController.m
+//  CarLot
+//
+//  Created by HIROKI IKEUCHI on 2019/02/21.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import "PreferenceController.h"
+
+@interface PreferenceController ()
+
+@end
+
+@implementation PreferenceController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    [super windowDidLoad];
+    NSLog(@"Nib file is loaded");
+}
+
+- (id)init {
+    self = [super initWithWindowNibName:@"Preferences"];
+    return self;
+}
+
+- (IBAction)changeBackgroundColor:(id)sender {
+    NSColor *color = [colorWell color];
+    NSLog(@"Color changed:%@", color);
+}
+
+- (IBAction)changeNewEmptyDoc:(id)sender {
+    NSInteger state = [checkbox state];
+    NSLog(@"Checkbox changed %ld", state);
+}
+
+
+@end

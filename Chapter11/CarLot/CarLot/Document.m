3ecbc1321e435ba0f00a6bd6c9669b96bc6e0c69
hikeuchi <hikeuchi@morisawa.co.jp>
2019-02-21 10:38:27 +0900

    P220

---
+++ Chapter11/CarLot/CarLot/Document.m
@@ -0,0 +1,37 @@
+//
+//  Document.m
+//  CarLot
+//
+//  Created by HIROKI IKEUCHI on 2019/02/20.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import "Document.h"
+
+@interface Document ()
+
+@end
+
+@implementation Document
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        // Add your subclass-specific initialization here.
+    }
+    return self;
+}
+
++ (BOOL)autosavesInPlace {
+    return YES;
+}
+
+
+- (NSString *)windowNibName {
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"Document";
+}
+
+
+@end

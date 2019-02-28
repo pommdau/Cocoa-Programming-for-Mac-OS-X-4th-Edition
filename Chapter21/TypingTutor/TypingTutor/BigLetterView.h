4268ae9020841685a201dd996ef8460de58fef6e
hikeuchi <hikeuchi@morisawa.co.jp>
2019-02-28 11:26:55 +0900

    copy project

---
+++ Chapter21/TypingTutor/TypingTutor/BigLetterView.h
@@ -0,0 +1,24 @@
+//
+//  BigLetterView.h
+//  TypingTutor
+//
+//  Created by Adam Preble on 9/21/11.
+//  Copyright (c) 2011 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BigLetterView : NSView {
+	NSColor *bgColor;
+	NSString *string;
+    NSMutableDictionary *attributes;    // フォント属性のDictionaryを保持する
+    BOOL isBold;
+    BOOL isItalic;
+}
+- (void)prepareAttributes;  // prepare attributes, font and fontColor
+@property (strong) NSColor *bgColor;
+@property (copy) NSString *string;
+- (IBAction)toggleBold:(NSButton *)sender;
+- (IBAction)toggleItalic:(NSButton *)sender;
+
+@end
